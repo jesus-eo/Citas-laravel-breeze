9ec1d1da221cadf99e21250a562183ebc19598fc
Jesús Manuel Espinar Ocaña <jesusmanuel.espinar@iesdonana.org>
2022-02-07 17:40:18 +0100

    cambio logging.php

--- config/logging.php
+++ config/logging.php
@@ -54,10 +54,14 @@ return [
             'ignore_exceptions' => false,
         ],
 
-        'single' => [
+       /*  'single' => [
             'driver' => 'single',
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
+        ], */
+        'single' => [
+            'driver' => 'errorlog',
+            'level' => 'debug',
         ],
 
         'daily' => [
